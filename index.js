67016e76a5bfaca7aa3eb68d63f576e624a651a4
Vinod Krishnan <vin.sinin@gmail.com>
2017-09-21 20:19:53 -0700

    stack addition

--- index.js
+++ index.js
@@ -16,6 +16,25 @@ var exports = {
          for(var i=0;i<nums.length;i++)
            m = Math.min(m,nums[i]);
          return m;
+    },
+    stack : {
+      arr : [],
+      size : 0,
+      push : function(value){
+        this.arr.unshift(value);
+        this.size++;
+      },
+      pop : function(){
+        this.arr.shift();
+        if(this.size > 0)
+          this.size--;
+      },
+      peek : function(){
+        return this.arr[0];
+      },
+      empty : function(){
+        return (this.size === 0);
+      }
     }
 }
 module.exports = exports;
--- test/test.js
+++ test/test.js
@@ -13,4 +13,56 @@ var helper = require('../index');
         var result = helper.max([1,2,3,4,5]);
         expect(result).to.equal(5);
     });
+
+    it('stack push 1 and check peek == 1', function() {
+        helper.stack.push(1);
+        var result = helper.stack.peek();
+        expect(result).to.equal(1);
+    });
+
+    it('stack push 2 and check peek == 2', function() {
+        helper.stack.push(2);
+        var result = helper.stack.peek();
+        expect(result).to.equal(2);
+    });
+
+    it('stack push 3 and check peek == 3', function() {
+        helper.stack.push(3);
+        var result = helper.stack.peek();
+        expect(result).to.equal(3);
+    });
+
+    it('stack empty check', function() {
+        var result = helper.stack.empty();
+        expect(result).to.equal(false);
+    });
+
+    it('stack pop and check peek == 2', function() {
+        helper.stack.pop();
+        var result = helper.stack.peek();
+        expect(result).to.equal(2);
+    });
+
+    it('stack pop and check peek == 1', function() {
+        helper.stack.pop();
+        var result = helper.stack.peek();
+        expect(result).to.equal(1);
+    });
+
+    it('stack pop and check peek == undefined', function() {
+        helper.stack.pop();
+        var result = helper.stack.peek();
+        expect(result).to.equal(undefined);
+    });
+
+    it('stack pop and check peek == undefined', function() {
+        helper.stack.pop();
+        var result = helper.stack.peek();
+        expect(result).to.equal(undefined);
+    });
+
+    it('stack empty check', function() {
+        var result = helper.stack.empty();
+        expect(result).to.equal(true);
+    });
 });
